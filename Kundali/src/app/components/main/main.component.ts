1da19b948ea45fdfe5a1ad5f54e49ea51a6210c2
Keshav <kkeshavpipar@gmail.com>
2023-12-14 07:29:51 +0530

    added form for client

--- Kundali/src/app/components/main/main.component.ts
+++ Kundali/src/app/components/main/main.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialComponentModule } from '../../material-component.module';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { MyErrorStateMatcher } from '../../customItems/formFieldErros';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MaterialComponentModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
+
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+
+  clientForm = this.fb.group({
+    name: ['', [Validators.required]],
+    age: ['', Validators.required],
+    country: ['', Validators.required],
+    city: ['', Validators.required],
+    state: ['', Validators.required],
+    birthDate: ['', Validators.required],
+    birthTime: ['', Validators.required]
+  })
+
+  constructor(private _errStateMatcher: MyErrorStateMatcher, private fb: FormBuilder) { }
+
+  matcher = new MyErrorStateMatcher();
+
+  saveForm() {
+    console.log(this.clientForm.value);
+  }
+
+  resetClientForm() {
+    this.clientForm.reset();
+  }
 
 }
